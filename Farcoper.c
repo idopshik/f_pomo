23a0e24d29c042bad558955966426896eed6eea0
idopshik <idopshik@gmail.com>
2020-01-22 14:51:14 +0300

    retab and fixed spaces

--- Farcoper.c
+++ Farcoper.c
@@ -26,9 +26,9 @@
 #define BAUD 9600
 
 
-#define ModeShowTomatos	     				1
-#define ModeTomatoActive					2
-#define ModeClockSet						3
+#define ModeShowTomatos                     1
+#define ModeTomatoActive                    2
+#define ModeClockSet                        3
 
 #define uart_new_line_Macro {uart_putc(0x0A); uart_putc(0x0D);}
 //---------------Переменные глобальные-------------------//
@@ -52,9 +52,9 @@ unsigned char const BeepPattern[] PROGMEM = {70,0,0};
 //unsigned char const BeepPattern_LONG[] PROGMEM = {250,1,250,1,0,0};
 unsigned char const BeepPattern_DoubleShort[] PROGMEM ={50,20,50,20,50,20,50,20,50,20,50,20,50,20,50,20,0,0};
 unsigned char const BeepPattern_DoubleShort_Wrong[] PROGMEM = {100,30,100,30,100,30,100,30,0,0};
-unsigned char const BeepPattern_Meloidic[] PROGMEM = {200,30,200,254,100,20,100,1,0,0};	
+unsigned char const BeepPattern_Meloidic[] PROGMEM = {200,30,200,254,100,20,100,1,0,0};
 unsigned char const BeepPattern_SwitchOn[] PROGMEM = {80,250,40,250,40,200,40,150,40,150,40,100,40,100,40,40,40,40,20,20,15,20,10,0};
-unsigned char const BeepPattern_SwitchOFF[] PROGMEM = {10,10,15,20,20,10,40,40,40,40,40,100,40,100,40,150,40,150,40,200,40,250,40,250,80, 0};		
+unsigned char const BeepPattern_SwitchOFF[] PROGMEM = {10,10,15,20,20,10,40,40,40,40,40,100,40,100,40,150,40,150,40,200,40,250,40,250,80, 0};
 
 unsigned char * BeeperPattern_pointer;     // Адресс массива
 unsigned char BeeperFlag;
@@ -83,68 +83,68 @@ void Check_Tomato_time (void);
 
 void Beeper_Activator(const uint8_t *pattern)
 {
-	BeeperPattern_pointer=(uint8_t *)pattern;
-	BeeperFlag|= 0x80;
-	BeeperPatternCounter = 0;
+    BeeperPattern_pointer=(uint8_t *)pattern;
+    BeeperFlag|= 0x80;
+    BeeperPatternCounter = 0;
 }
 
 void Deal_with_Beeper(void)
 {
-	uint8_t var;
-	if ( BeeperFlag & 0x80)
-	{
-		var = pgm_read_byte(BeeperPattern_pointer+BeeperPatternCounter);
-		if (var == 0)
-		{BeeperFlag = 0;
-		BeeperPattern_pointer = 0;
-		BeeperPatternCounter=0;
-		Buzzer_OFF;
-		return;
-		}
-		else if(BeeperFlag&0x01)
-		{
-			Buzzer_OFF;
-			BeeperFlag&=0xFE;
-		}
-		else
-		{
-			Buzzer_ON;
-			BeeperFlag|=0x01;
-		}
-		BeepDuration = var;
-		BeeperPatternCounter++;
-	}
+    uint8_t var;
+    if ( BeeperFlag & 0x80)
+    {
+        var = pgm_read_byte(BeeperPattern_pointer+BeeperPatternCounter);
+        if (var == 0)
+        {BeeperFlag = 0;
+        BeeperPattern_pointer = 0;
+        BeeperPatternCounter=0;
+        Buzzer_OFF;
+        return;
+        }
+        else if(BeeperFlag&0x01)
+        {
+            Buzzer_OFF;
+            BeeperFlag&=0xFE;
+        }
+        else
+        {
+            Buzzer_ON;
+            BeeperFlag|=0x01;
+        }
+        BeepDuration = var;
+        BeeperPatternCounter++;
+    }
 }
 
 
 
-ISR(TIMER0_OVF_vect)		// 2ms  // Обслуживаем индикацию
+ISR(TIMER0_OVF_vect)        // 2ms  // Обслуживаем индикацию
 {
-	G_counter_char++;
-	
-	Within_ISR_button_service();
-	
-	 Digit++;												//изменение разряда для мультиплексированного вывода.
-	 if(Digit==4) Digit=0;
-	 
-	 if(10>1000) PutOneDigit(10,Digit,0);				// 190 ms чёрный экран - моргание. Не выполняется.
-	 else
-	 {
+    G_counter_char++;
+
+    Within_ISR_button_service();
+
+     Digit++;                                               //изменение разряда для мультиплексированного вывода.
+     if(Digit==4) Digit=0;
+
+     if(10>1000) PutOneDigit(10,Digit,0);               // 190 ms чёрный экран - моргание. Не выполняется.
+     else
+     {
         if((Operation_byte == ModeClockSet)||(Operation_byte == ModeTomatoActive)){
              if ((Digit==2)&&(G_counter_char>127)) PutOneDigit(LED_string[Digit],Digit,1);
-             else PutOneDigit(LED_string[Digit],Digit,0);	  		 
+             else PutOneDigit(LED_string[Digit],Digit,0);
         }
-	
+
         if(Operation_byte == ModeShowTomatos){
-                if((Digit==0) || (Digit == 1))PutOneDigit(LED_string[Digit],Digit,0);	  		 
+                if((Digit==0) || (Digit == 1))PutOneDigit(LED_string[Digit],Digit,0);
                 else PutOneDigit(10,Digit,0);    //не показывать лишние разряды.
         }
-        
+
     }
      if(Operation_byte == ModeTomatoActive){
         Led_RED_ON;
 
-		//Попеременно светодиодами
+        //Попеременно светодиодами
         /* if(G_counter_char==0) */
         /* {Led_Yellow_OFF;Led_RED_ON;} */
         /* if(G_counter_char==127) */
@@ -155,53 +155,53 @@ ISR(TIMER0_OVF_vect)		// 2ms  // Обслуживаем индикацию
      }
 }
 
-ISR(TIMER2_OVF_vect)					//1ms // Логика датчика и задержек.
+ISR(TIMER2_OVF_vect)                    //1ms // Логика датчика и задержек.
 {
-	
+
     G_time1++;
-    if(G_time1>1000)					// Секунда
+    if(G_time1>1000)                    // Секунда
     {
-	
-		// Таймер для зуммера	
-		if(BeepDuration>0)BeepDuration--;
-		if(BeepDuration ==0)Deal_with_Beeper();
 
-		G_counter++;
+        // Таймер для зуммера
+        if(BeepDuration>0)BeepDuration--;
+        if(BeepDuration ==0)Deal_with_Beeper();
 
-	}
+        G_counter++;
+
+    }
 
 
-	if (G_counter > 100) // каждые 200ms
-	{
+    if (G_counter > 100) // каждые 200ms
+    {
                 if(Operation_byte == ModeShowTomatos)f = Tomatos_to_string;
-				Nested_counter++;
-				if (Nested_counter >3)
-				{
+                Nested_counter++;
+                if (Nested_counter >3)
+                {
                     if(Operation_byte == ModeClockSet)f = Get_time;
-                    if(Operation_byte == ModeTomatoActive)f = Check_Tomato_time; 
-					Nested_counter =0;
-					Last_GlobalVar = 1;	
-                }			
+                    if(Operation_byte == ModeTomatoActive)f = Check_Tomato_time;
+                    Nested_counter =0;
+                    Last_GlobalVar = 1;
+                }
 
-                
 
-		G_counter =0;
-	}	
+
+        G_counter =0;
+    }
 }
 
 
 inline void setupTimer_0 (void)
-{			//переполнение каждые 2,048мс
-	TCCR0 = 0;
-	TCCR0 |= (1<<CS01)|(1<<CS00);	//64, 32 у этого таймера нет.
-	TIMSK|=(1<<TOIE0);            // Enable Counter Overflow Interrupt
+{           //переполнение каждые 2,048мс
+    TCCR0 = 0;
+    TCCR0 |= (1<<CS01)|(1<<CS00);   //64, 32 у этого таймера нет.
+    TIMSK|=(1<<TOIE0);            // Enable Counter Overflow Interrupt
 }
 
 inline void SetupTimer_2 (void)
-{			//переполнение каждые 1,024мс
-	TCCR2 = 0;
-	TCCR2 |= (1<<CS21)|(1<<CS20);	//32
-	TIMSK|=(1<<TOIE2);            // Enable Counter Overflow Interrupt
+{           //переполнение каждые 1,024мс
+    TCCR2 = 0;
+    TCCR2 |= (1<<CS21)|(1<<CS20);   //32
+    TIMSK|=(1<<TOIE2);            // Enable Counter Overflow Interrupt
 }
 
 
@@ -209,20 +209,20 @@ inline void SetupTimer_2 (void)
 
 
 void Get_time (void)
-{	
+{
     uint8_t var,hour,minute,second;
 
     ds1307_getdate(&var, &var, &var, &hour, &minute, &second);
-    
+
     LED_string[3]=(hour % 100 / 10);
     LED_string[2]=(hour % 10);
     LED_string[1]=(minute % 100 / 10);
-    LED_string[0]=(minute % 10);	
+    LED_string[0]=(minute % 10);
 }
 
 uint8_t old_second = 0;
 void Check_Tomato_time (void)
-{	
+{
     uint8_t var,second;
 
     ds1307_getdate(&var, &var, &var, &var, &var, &second);
@@ -230,13 +230,13 @@ void Check_Tomato_time (void)
     if (second != old_second){
         old_second=second;
 
-        uart_puts(" Second_to_count :" );  
+        uart_puts(" Second_to_count :" );
         uart_put_int(Second_to_count);    uart_new_line_Macro
 
         if(Second_to_count)Second_to_count--;
         else{
 
-            DoneTomatos++; 
+            DoneTomatos++;
             eeprom_write_byte(&EE_Done_Tomatos,DoneTomatos);                          //всего десять уровней заполнения
             Operation_byte = ModeShowTomatos;
             Beeper_Activator(BeepPattern_SwitchOFF);
@@ -251,11 +251,11 @@ void Check_Tomato_time (void)
 
     var = Second_to_count % 60;
     LED_string[1]=(var % 100 / 10);
-    LED_string[0]=(var % 10);	
+    LED_string[0]=(var % 10);
 }
 
 void Get_time_debug (void)
-{	
+{
     uint8_t var,hour,minute,second;
 
     ds1307_getdate(&var, &var, &var, &hour, &minute, &second);
@@ -269,39 +269,39 @@ void Get_time_debug (void)
     uart_puthex_byte(minute);    uart_new_line_Macro
     uart_puthex_byte(hour);    uart_new_line_Macro
 
-    
+
 
 
 }
 
 uint16_t DIG_digit(uint16_t dig, uint16_t sub,uint8_t DIGIT) {
-	char c = 0;
-	while (dig >= sub) {
-		dig -= sub;
-		c++;
-	}
-	LED_string[DIGIT]=c;				// Вожделенная строчка!
-	return dig;
+    char c = 0;
+    while (dig >= sub) {
+        dig -= sub;
+        c++;
+    }
+    LED_string[DIGIT]=c;                // Вожделенная строчка!
+    return dig;
 }
 
 
 void Tomatos_to_string(void)
 {
     LED_string[1]=(DoneTomatos % 100 / 10);
-    LED_string[0]=(DoneTomatos % 10);	
+    LED_string[0]=(DoneTomatos % 10);
 }
 
 void DIG_num(int16_t num) {
-	uint16_t unum; // число без знака
+    uint16_t unum; // число без знака
 
-	unum = num;
-	uint16_t snum =  unum >> 4; // отбрасывает дробную часть
-	if (snum >= 10) {
-		LED_string[3]=10;						 // Нафиг не нужен этот разряд в комнате.
-		snum = DIG_digit(snum, 10,2); // 2й разряд
-	}
-	DIG_digit(snum, 1,1); // 1й разряд
-	DIG_digit((((uint8_t)(unum & 0x0F)) * 10) >> 4, 1,0); // дробная часть
+    unum = num;
+    uint16_t snum =  unum >> 4; // отбрасывает дробную часть
+    if (snum >= 10) {
+        LED_string[3]=10;                        // Нафиг не нужен этот разряд в комнате.
+        snum = DIG_digit(snum, 10,2); // 2й разряд
+    }
+    DIG_digit(snum, 1,1); // 1й разряд
+    DIG_digit((((uint8_t)(unum & 0x0F)) * 10) >> 4, 1,0); // дробная часть
 }
 
 
@@ -309,20 +309,20 @@ void DIG_num(int16_t num) {
 
 
  int main (void)
- 
+
  {
-	wdt_enable(WDTO_500MS);
+    wdt_enable(WDTO_500MS);
 
     DoneTomatos = eeprom_read_byte(&EE_Done_Tomatos);
-	 
 
-	 	 //////////////////////////   H A R D W A R E   I N I T   /////////////////////////////////
+
+         //////////////////////////   H A R D W A R E   I N I T   /////////////////////////////////
 Hardware_set_for_shift();
 Hardware_init();
-Buzzer_ON;  
-sei();		  
+Buzzer_ON;
+sei();
 
-	
+
 _delay_ms(3);
 setupTimer_0(); // LED и опрос кнопок
 SetupTimer_2(); // Работа с акселерометром
@@ -342,36 +342,36 @@ uart_puts("UART_DEBUG_IS_ACTIVE !");  uart_new_line_Macro
 #endif
 
 
-uart_puts("DoneTomatos - ");  
+uart_puts("DoneTomatos - ");
 uart_put_int(DoneTomatos);    uart_new_line_Macro
-uart_puts("Loading_Complete"); uart_new_line_Macro 
+uart_puts("Loading_Complete"); uart_new_line_Macro
 
-    
-	while(1)
-	{
-		wdt_reset(); // сбрасываем собачий таймер
 
-		  switch(ButtonCheck())
-		  {
-			  case ButtonPressed_0_MASK:
-			  Beeper_Activator(BeepPattern);
+    while(1)
+    {
+        wdt_reset(); // сбрасываем собачий таймер
+
+          switch(ButtonCheck())
+          {
+              case ButtonPressed_0_MASK:
+              Beeper_Activator(BeepPattern);
 #if defined( DebugCommon )
               Get_time_debug();
               uart_puts("Button_1_short_Pressed" );  uart_new_line_Macro
 #endif
-			  break;
-			  case ButtonPressed_1_MASK:                        //red button
-			  Beeper_Activator(BeepPattern);
+              break;
+              case ButtonPressed_1_MASK:                        //red button
+              Beeper_Activator(BeepPattern);
 
-			  break;
-			  case ButtonPressed_0_LONG_MASK:                   // green button
-			  Beeper_Activator(BeepPattern_Meloidic);
+              break;
+              case ButtonPressed_0_LONG_MASK:                   // green button
+              Beeper_Activator(BeepPattern_Meloidic);
 
               Operation_byte = ModeTomatoActive;
               Second_to_count = 15;
               //для дебага
-			  break;
-			  case ButtonPressed_1_LONG_MASK:
+              break;
+              case ButtonPressed_1_LONG_MASK:
 
 #if defined( DebugCommon )
               Get_time_debug();
@@ -381,30 +381,30 @@ uart_puts("Loading_Complete"); uart_new_line_Macro
               Operation_byte = ModeTomatoActive;
               Second_to_count = 1500;
 #endif
-			  Beeper_Activator(BeepPattern_Meloidic);
+              Beeper_Activator(BeepPattern_Meloidic);
 
-			  break;
-			  case (ButtonPressed_0_LONG_MASK| ButtonPressed_1_LONG_MASK):
-			  
-            DoneTomatos = 0; 
+              break;
+              case (ButtonPressed_0_LONG_MASK| ButtonPressed_1_LONG_MASK):
+
+            DoneTomatos = 0;
             eeprom_write_byte(&EE_Done_Tomatos,DoneTomatos);                          //всего десять уровней заполнения
-			  break;
-			  case ButtonPressed_SHORT_Double_MASK:
-		
+              break;
+              case ButtonPressed_SHORT_Double_MASK:
+
 #if defined( DebugCommon )
                 uart_puts("Buttons_both_short_pressed" );  uart_new_line_Macro
 #endif
                     Operation_byte = ModeClockSet;
-			  break;
-		
-		  }
+              break;
+
+          }
 
         if(f)
         {
             cli();
-            f();	// вызов функции если там есть чё.
+            f();    // вызов функции если там есть чё.
             f=NULL;   // Предотвращаем цикличность.
             sei();
         }
-	}
+    }
  }
